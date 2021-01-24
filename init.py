a334c802b62ee32a0f2049379fd8f110587ab9e8
Enis Kollugil <enis.kollugil@hepsiburada.com>
2021-01-24 15:51:02 +0300

    add deny

--- init.py
+++ init.py
@@ -10,11 +10,11 @@ def webhook():
     allowed = True
     request_info = request.json
     pprint(request_info)
-    # # for container_spec in request_info
-    # for container_spec in request_info["request"]["object"]["spec"]["containers"]:
-    #     if 'env' in container_spec:
-    #         print("Environment Variables Cannot Be Passed to Containers")
-    #         allowed = False
+     # for container_spec in request_info
+    for container_spec in request_info["request"]["object"]["spec"]["containers"]:
+         if 'env' not in container_spec:
+             print("Environment Variables Cannot Be Passed to Containers")
+             allowed = False
 
     admission_response = {
         "allowed": allowed
